aa16f5331b5370489e56700138cb91b4d8fcdce0
TOM <89062432+TOM69JK@users.noreply.github.com>
2023-05-08 23:23:59 +0800

    Update san.js

--- san.js
+++ san.js
@@ -35,7 +35,7 @@ const UA = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (
 
   ;(async () => {
     let panel_result = {
-      title: '🖥🅢🅣🅡🅔🅐🅜🅘🅝🅖🚥',
+      title: '🅢🅣🅡🅔🅐🅜🅘🅝🅖',
       content: '',
       icon: 'play.tv.fill',
       'icon-color': '#FF0097',
